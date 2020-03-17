e5d1c1deabad50096c3628c4834eccb30b4330ec
Elizandro Dalasra <elizandro.dalastra@compasso.com.br>
2020-03-17 14:55:35 -0300

    atualizando

--- app/back_end/workshop.sql
+++ app/back_end/workshop.sql
@@ -35,35 +35,38 @@ create table scheduling (
     datatime timestamp not null,
     description text not null,
     ps text,
-    service_id int not null,
+    service_area_id int not null,
+    service_type_id int not null ,
     vehicle_id int not null
 );
 
-create table service (
-    id serial primary key ,
-    area varchar(10) check ( area in ('motor', 'eletrica', 'suspenção')),
-    type_id int not null ,
-    name varchar(100) not null,
-    description text,
-    base_value money
+create table service_area (
+    id smallint primary key ,
+    name varchar(30) not null
 );
-drop table service;
 
-table service;
+create table service_type (
+    id smallint primary key ,
+    name varchar(10) not null
+);
+
+
 create table vehicle (
     id serial primary key ,
     placa char(7) not null check ( placa ~ '^[A-Z]{3}[A-Z0-9]{4}$') unique ,
     model_id int not null ,
     year char(4) not null,
+    description varchar(100),
     owner_id int not null
 );
-drop table vehicle cascade ;
-
 insert into vehicle (placa, model_id, year, owner_id) values ('IKP3J96',194,'2002',1);
+insert into service_type values (1,'preventiva'), (2,'corretiva');
+insert into service_area values (1,'motor'), (2,'eletrica'), (3,'suspenção'),(4,'escapamento');
 
 alter table person add foreign key (city_id) references city;
-alter table scheduling add foreign key (service_id) references service;
 alter table scheduling add foreign key (vehicle_id) references vehicle;
+alter table scheduling add foreign key (service_area_id) references service_area;
+alter table scheduling add foreign key (service_type_id) references service_type;
 alter table vehicle add foreign key (owner_id) references person;
 alter table model  add foreign key (brand_id) references brand ;
 alter table vehicle add foreign key (model_id) references model;
@@ -71,5 +74,6 @@ alter table city add foreign key (state_id) references state;
 
 
 
+delete from vehicle;
 
-SELECT * FROM model WHERE brand_id = 1;
+table vehicle;
